ed9df6d3b4aebbcaa520ab7fb44b4dc4655c75f5
JoshuaSandag <s21810416@student.unklab.ac.id>
2020-12-06 15:38:24 +0700

    7 : Menggunakan package react number format

--- covid-19-tracker/src/card/Global/index.js
+++ covid-19-tracker/src/card/Global/index.js
@@ -1,11 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import "../../App.css";
+import NumberFormat from "react-number-format";
 
 function Card (){
-    const [confirmed, setConfirmed] = useState([]);
-    const [deaths, setDeaths] = useState([]);
-    const [recovered, setRecovered] = useState([]);
+    const [confirmed, setConfirmed] = useState("");
+    const [deaths, setDeaths] = useState("");
+    const [recovered, setRecovered] = useState("");
     useEffect(() => {
       axios 
        .get("https://covid19.mathdro.id/api")
@@ -15,17 +16,25 @@ function Card (){
        setDeaths(response.data.deaths.value);
        setRecovered(response.data.recovered.value);
       })  
-   }, []);
+    }, []);
   
     return(
     <div className = "box">
-  
-      <h1 className ="card-container1">Positif {confirmed}</h1>
-      <h1 className ="card-container2">Meninggal {deaths}</h1>
-      <h1 className ="card-container3">Sembuh {recovered}</h1>
-  
-    </div>
+      <h1 className ="card-container1">
+        <h4>Positif</h4> 
+        <NumberFormat value = {confirmed} thousandSeparator={true} displayType ={'text'}/>
+      </h1>
 
+      <h1 className ="card-container2">
+        <h4>Meninggal</h4> 
+        <NumberFormat value = {deaths} thousandSeparator={true} displayType ={'text'}/>
+      </h1>
+
+      <h1 className ="card-container3">
+        <h4>Sembuh</h4> 
+        <NumberFormat value = {recovered} thousandSeparator={true} displayType ={'text'}/>
+      </h1>
+    </div>
   );    
 };
 
--- covid-19-tracker/src/card/Indonesia/index.js
+++ covid-19-tracker/src/card/Indonesia/index.js
@@ -1,11 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState} from "react";
 import axios from "axios";
 import "../../App.css";
+import NumberFormat from "react-number-format";
 
 function Card  (){
-    const [confirmed, setConfirmed] = useState([]);
-    const [deaths, setDeaths] = useState([]);
-    const [recovered, setRecovered] = useState([]);
+    const [confirmed, setConfirmed] = useState("");
+    const [deaths, setDeaths] = useState("");
+    const [recovered, setRecovered] = useState("");
     useEffect(() => {
       axios 
        .get("https://indonesia-covid-19.mathdro.id/api")
@@ -18,14 +19,23 @@ function Card  (){
    }, []);
   
     return(
-    <div className = "box">
-  
-      <h1 className ="card-container1">Positif {confirmed}</h1>
-      <h1 className ="card-container2">Meninggal {deaths}</h1>
-      <h1 className ="card-container3">Sembuh {recovered}</h1>
-  
-    </div>  
-  );    
+      <div className = "box">
+      <h1 className ="card-container1">
+        <h4>Positif</h4> 
+        <NumberFormat value = {confirmed} thousandSeparator={true} displayType ={'text'}/>
+      </h1>
+
+      <h1 className ="card-container2">
+        <h4>Meninggal</h4> 
+        <NumberFormat value = {deaths} thousandSeparator={true} displayType ={'text'}/>
+      </h1>
+
+      <h1 className ="card-container3">
+        <h4>Sembuh</h4> 
+        <NumberFormat value = {recovered} thousandSeparator={true} displayType ={'text'}/>
+      </h1>
+    </div>
+    );    
 };
 
 export default Card;
